66c9b0cb7f6ed7d0eb8f1e220050cc1621290aff
shubhamm1267 <shubhammahajan1267@gmail.com>
2025-02-07 11:18:57 +0530

    updated changes on 7feb

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,36 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule,FormsModule,ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent implements OnInit {
   sidebarOpen = true;
-  tabIndex=0;
-  userList:any[]= [
-    {id:"CI17211",fname:"Sagar",username:"sagar@cloverinfoech.com",role:"Frontend",status:"Active"},
-    {id:"CI17212",fname:"Shubham",username:"shubham@cloverinfoech.com",role:"Backend",status:"Inactive"},
-    {id:"CI17213",fname:"Avesh",username:"avesh@cloverinfoech.com",role:"Backend",status:"Active"},
-    {id:"CI17214",fname:"Aman",username:"aman@cloverinfoech.com",role:"Backend",status:"Inactive"},
-    {id:"CI17215",fname:"Panu",username:"panu@cloverinfoech.com",role:"Backend",status:"Inactive"},
-    {id:"CI17216",fname:"Harsh",username:"harsh@cloverinfoech.com",role:"Frontend",status:"Active"}
-  ];
-  studentList:any[]= [
-    {id:"CI17211",licence:"Pune",fname:"sagar",mobile:8830267423,status:"Active"},
-    {id:"CI17212",licence:"Mumbai",fname:"shubham",mobile:7830267423,status:"Inactive"},
-    {id:"CI17213",licence:"Delhi",fname:"avesh",mobile:6768547439,status:"Active"},
-    {id:"CI17214",licence:"Kolkata",fname:"aman",mobile:9412474697,status:"Inactive"},
-    {id:"CI17215",licence:"Jalgaon",fname:"panu",mobile:9422308532,status:"Inactive"},
-    {id:"CI17216",licence:"Pune",fname:"harsh",mobile:7712870733,status:"Active"}
-  ];
+  tabIndex = 0;
+  userList: any[] = [];
+  studentList: any[] = [];
+  
+  showToast = false;
+  selectedUser: any = null;
 
-  filteredUserList:any = [...this.userList];
-  filteredStudentList:any = [...this.studentList];
+  filteredUserList: any = [...this.userList];
+  filteredStudentList: any = [...this.studentList];
 
   searchText = '';
 
@@ -52,24 +47,77 @@ export class DashboardComponent implements OnInit {
   isEdit = false;
   selectedIndex: number | null = null;
   isModalOpen = false;
-  constructor(private router: Router) {}
-
+  currentPage = 1;
+  itemsPerPage = 8;
+  constructor(private router: Router, private userService: UserService) {}
 
   ngOnInit() {
-  
+    this.loadUsers();
   }
 
-  setTabIndex(tabIndex:number){
+  loadUsers() {
+    this.userService.getUsers().subscribe((users) => (this.studentList = users));
+    console.log('users========>>>>>>>>>>>', this.studentList);
+  }
+
+  addUser() {
+    this.userService.addUser(this.studentForm.value).subscribe(() => {
+      this.showNotification();
+      this.loadUsers();
+    });
+  }
+
+  updateUser() {
+    if (this.selectedUser) {
+      this.userService.updateUser(this.selectedUser._id, this.studentForm.value).subscribe(() => {
+        this.showNotification();
+        this.loadUsers();
+      });
+    }
+  }
+
+
+  showNotification() {
+    this.showToast = true;
+    setTimeout(() => this.showToast = false, 3000);
+  }
+
+  setTabIndex(tabIndex: number) {
     this.tabIndex = tabIndex;
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.studentList.length / this.itemsPerPage);
+  }
+
+  get paginatedList() {
+    let start = (this.currentPage - 1) * this.itemsPerPage;
+    return this.studentList.slice(start, start + this.itemsPerPage);
+  }
+
+  changePage(page: number) {
+    this.currentPage = page;
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
   }
 
   toggleSidebar() {
     this.sidebarOpen = !this.sidebarOpen;
   }
 
-  logout(){
-   this.router.navigate(['/login'])
-   localStorage.removeItem("registerData");
+  logout() {
+    this.router.navigate(['/login']);
+    localStorage.removeItem('registerData');
   }
 
   openModal(index: any | null = null) {
@@ -79,7 +127,7 @@ export class DashboardComponent implements OnInit {
     if (this.isEdit) {
       this.userForm.setValue(this.filteredUserList[index]);
     } else {
-      this.userForm.reset({ status: 'Select Status',role:'Select Role' });
+      this.userForm.reset({ status: 'Select Status', role: 'Select Role' });
     }
 
     this.isModalOpen = true;
@@ -92,7 +140,7 @@ export class DashboardComponent implements OnInit {
     if (this.isEdit) {
       this.studentForm.setValue(this.filteredStudentList[index]);
     } else {
-      this.studentForm.reset({ status: 'Select Status'});
+      this.studentForm.reset({ status: 'Select Status' });
     }
 
     this.isModalOpen = true;
@@ -104,26 +152,27 @@ export class DashboardComponent implements OnInit {
 
   filterUsers() {
     const text = this.searchText.toLowerCase();
-    this.filteredUserList = this.userList.filter(user =>
-      user.id.toLowerCase().includes(text) ||
-      user.fname.toLowerCase().includes(text) ||
-      user.username.toLowerCase().includes(text) ||
-      user.role.toLowerCase().includes(text) ||
-      user.status.toLowerCase().includes(text)
+    this.filteredUserList = this.userList.filter(
+      (user) =>
+        user.id.toLowerCase().includes(text) ||
+        user.fname.toLowerCase().includes(text) ||
+        user.username.toLowerCase().includes(text) ||
+        user.role.toLowerCase().includes(text) ||
+        user.status.toLowerCase().includes(text)
     );
   }
 
-  filterStudents(){
+  filterStudents() {
     const text = this.searchText.toLowerCase();
-    this.filteredUserList = this.studentList.filter(user =>
-      user.id.toLowerCase().includes(text) ||
-      user.licence.toLowerCase().includes(text) ||
-      user.fname.toLowerCase().includes(text) ||
-      user.mobile.toLowerCase().includes(text) ||
-      user.status.toLowerCase().includes(text)
+    this.filteredUserList = this.studentList.filter(
+      (user) =>
+        user.id.toLowerCase().includes(text) ||
+        user.licence.toLowerCase().includes(text) ||
+        user.fname.toLowerCase().includes(text) ||
+        user.mobile.toLowerCase().includes(text) ||
+        user.status.toLowerCase().includes(text)
     );
   }
-  
 
   onSubmit() {
     if (this.isEdit && this.selectedIndex !== null) {
@@ -131,27 +180,28 @@ export class DashboardComponent implements OnInit {
     } else {
       this.userList.push(this.userForm.value);
     }
-    this.filterUsers(); 
+    this.filterUsers();
     this.closeModal();
   }
 
-  onStudentSubmit(){
+  onStudentSubmit() {
     if (this.isEdit && this.selectedIndex !== null) {
       this.studentList[this.selectedIndex] = this.studentForm.value;
     } else {
       this.studentList.push(this.studentForm.value);
     }
-    this.filterStudents(); 
+    this.filterStudents();
     this.closeModal();
   }
 
   editUser(index: number) {
     this.openModal(index);
   }
-
-  editStudent(index: number) {
-    this.openStudentModal(index);
+  editStudent(user: any) {
+    this.selectedUser = user;
+    this.studentForm.patchValue(user);
   }
+ 
 
   deleteUser(index: number) {
     const originalIndex = this.userList.indexOf(this.filteredUserList[index]);
@@ -159,9 +209,8 @@ export class DashboardComponent implements OnInit {
     this.filterUsers();
   }
 
-  deleteStudent(index: number) {
-    const originalIndex = this.studentList.indexOf(this.filteredStudentList[index]);
-    this.studentList.splice(originalIndex, 1);
+  deleteStudent(id: any) {
+    this.userService.deleteUser(id).subscribe(() => this.loadUsers());
     this.filterUsers();
   }
 }
