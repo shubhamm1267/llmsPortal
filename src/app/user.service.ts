cb932b0c9c977d540292746bd1e2a06dd145e37f
shubh1267 <shubhammahajan1267@gmail.com>
2025-02-11 18:01:16 +0530

    added apiurl

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = "http://localhost:5000";
+  private apiUrl = "https://backend-llmsportal.onrender.com";
 
   constructor(private http: HttpClient) {}
 
